300b45aefdee63da4c972614737bebe3ba89814f
pragati1610 <pragatibhattad1610@gmail.com>
2021-04-03 09:07:56 +0530

    fix: lint errors

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,57 +1,5 @@
-const { User } = require('../models/relations')
-const logger = require('../logging/logger')
-
 class PostsController {
-  static async createTodo (todo, email) {
-    try {
-      const todoFound = await User.findAll({ where: { email: email } })
-      if (todoFound[0].todo == null) {
-        const arr = []
-        arr.push(todo)
-        todoFound[0].todo = arr
-        const val = {}
-        val.todo = todoFound[0].todo
-        const updatedUser = await User.update(val, { where: { email: email } })
-        return {
-          message: 'Added first todo',
-          updatedUser
-        }
-      }
-      const arr = todoFound[0].todo
-      arr.push(todo)
-      todoFound[0].todo = arr
-      const val = {}
-      val.todo = todoFound[0].todo
-      const updatedUser = await User.update(val, { where: { email: email } })
-      return { message: 'Added todo', updatedUser }
-    } catch (e) {
-      logger.error(e)
-      return {
-        isError: true,
-        message: e.toString()
-      }
-    }
-  }
 
-  static async getAllTodos (userId) {
-    try {
-      const user = await User.findByPk(userId)
-      console.log(user)
-      console.log(user.todo)
-      if ((user.todo) == null) {
-        return {
-          message: 'Empty'
-        }
-      }
-      return user.todo
-    } catch (e) {
-      logger.error(e)
-      return {
-        isError: true,
-        message: e.toString()
-      }
-    }
-  }
 }
 
 module.exports = PostsController
