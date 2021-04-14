4e3bf73e3f7f249f945cf4fd25332ab78ed93d2b
pragati1610 <pragatibhattad1610@gmail.com>
2021-04-14 20:53:42 +0530

    fix: add points and username to getAllLatestPosts route result

--- models/badge.js
+++ models/badge.js
@@ -6,7 +6,6 @@ const schema = {
   badgeName: { type: DataTypes.STRING, allowNull: false },
   hasStreak: { type: DataTypes.BOOLEAN },
   days: { type: DataTypes.INTEGER, allowNull: false },
-  badgeImgUrl: { type: DataTypes.STRING },
   upvotes: { type: DataTypes.INTEGER, allowNull: false, default: 0 }
 }
 
