19e78f806ffbfd6c10ba57f76989f6d46fb11a2b
pragati1610 <pragatibhattad1610@gmail.com>
2021-05-04 23:27:39 +0530

    feat: add inProgress and completed badges routes

--- routes/badgeRoute.js
+++ routes/badgeRoute.js
@@ -15,18 +15,14 @@ router.get('/', [jwtAuth], async (req, res) => {
   return res.status(response.isError ? 400 : 200).send(response)
 })
 
-router.get('/:badgeId', [jwtAuth], async (req, res) => {
+router.get('/getBadge/:badgeId', [jwtAuth], async (req, res) => {
   const response = await badge.getBadge(req.params.badgeId)
   return res.status(response.isError ? 400 : 200).send(response)
 })
 
 router.get('/inProgress', [jwtAuth], async (req, res) => {
+  console.log(req.claims.userId)
   const response = await badge.getInProgressBadge(req.claims.userId)
-  return res.status(response.isError ? 400 : 200).send(response)
-})
-
-router.get('/completed', [jwtAuth], async (req, res) => {
-  const response = await badge.getCompletedBadge(req.claims.userId)
   return res.status(response.isError ? 400 : 200).send(response)
 })
 
